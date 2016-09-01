5f15d0d56281635cb292f695ad4ba6aaeefb2545
Anthony Shalagin <anthony.shalagin@gmail.com>
2016-09-01 07:07:22 -0400

    Built functionality to add and delete movies

--- user.py
+++ user.py
@@ -1,3 +1,6 @@
+from movie import Movie
+
+
 class User:
     def __init__(self,name):
         self.name = name
@@ -5,6 +8,19 @@ class User:
 
     def __repr__(self):
         return "<User {}>".format(self.name)
+
+    def add_movie(self, name, genre):
+        movie = Movie(name,genre,False)
+        self.movies.append(movie)
+
+    def delete_movie(self, name):
+        self.movies = list(filter(lambda movie: movie.name != name, self.movies))
+
+        #Filter takes in self.movie, stores it in movie, and compares whether movie.name is equal to the name passed in
+
+        for movie in self.movies:
+            if movie.name == name:
+                self.movies.remove(self,name)
 
     def watched_movies(self):                                               #Get a list of movies that have been watched
         return list(filter(lambda movie: movie.watched, self.movies))       #Cast the filter to a list
